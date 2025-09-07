f08d984e8e476fb533160c079c27c77fc491034f
StevRocket <rocketlearningnetwork@gmail.com>
2025-09-07 11:08:11 -0400

    Update app-pub-googlesheet.py

--- app-pub-googlesheet.py
+++ app-pub-googlesheet.py
@@ -3,10 +3,12 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
+URL = st.secrets["GOOGLURL"]
+
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read()
+df = conn.read(spreadsheet=URL)
 
 # Print results.
 for row in df.itertuples():
